69d5162ec9957b7f30a5de6058e2ac10f9d778b8
vijay9655 <vijaypl9655@gmail.com>
2022-09-19 15:45:24 +0530

    screen share

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -11,10 +11,91 @@ const Room = (props) => {
     const senders = useRef([]);
 
     useEffect(() => {
-        callback()
-
-    }, []);
-    const callback=()=>{
+        const callUser=(userID)=> {
+            peerRef.current = createPeer(userID);
+            userStream.current.getTracks().forEach(track => senders.current.push(peerRef.current.addTrack(track, userStream.current)));
+        }
+    
+        const createPeer=(userID)=> {
+            const peer = new RTCPeerConnection({
+                iceServers: [
+                    {
+                        urls: "stun:stun.stunprotocol.org"
+                    },
+                    {
+                        urls: 'turn:numb.viagenie.ca',
+                        credential: 'muazkh',
+                        username: 'webrtc@live.com'
+                    },
+                ]
+            });
+    
+            peer.onicecandidate = handleICECandidateEvent;
+            peer.ontrack = handleTrackEvent;
+            peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
+    
+            return peer;
+        }
+    
+        const handleNegotiationNeededEvent=(userID)=> {
+            peerRef.current.createOffer().then(offer => {
+                return peerRef.current.setLocalDescription(offer);
+            }).then(() => {
+                const payload = {
+                    target: userID,
+                    caller: socketRef.current.id,
+                    sdp: peerRef.current.localDescription
+                };
+                socketRef.current.emit("offer", payload);
+            }).catch(e => console.log(e));
+        }
+    
+        const handleRecieveCall=(incoming)=> {
+            peerRef.current = createPeer();
+            const desc = new RTCSessionDescription(incoming.sdp);
+            peerRef.current.setRemoteDescription(desc).then(() => {
+                userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
+            }).then(() => {
+                return peerRef.current.createAnswer();
+            }).then(answer => {
+                return peerRef.current.setLocalDescription(answer);
+            }).then(() => {
+                const payload = {
+                    target: incoming.caller,
+                    caller: socketRef.current.id,
+                    sdp: peerRef.current.localDescription
+                }
+                socketRef.current.emit("answer", payload);
+            })
+        }
+    
+        const handleAnswer=(message)=> {
+            const desc = new RTCSessionDescription(message.sdp);
+            peerRef.current.setRemoteDescription(desc).catch(e => console.log(e));
+        }
+    
+        const handleICECandidateEvent=(e)=> {
+            if (e.candidate) {
+                const payload = {
+                    target: otherUser.current,
+                    candidate: e.candidate,
+                }
+                socketRef.current.emit("ice-candidate", payload);
+            }
+        }
+    
+        const handleNewICECandidateMsg=(incoming)=> {
+            const candidate = new RTCIceCandidate(incoming);
+    
+            peerRef.current.addIceCandidate(candidate)
+                .catch(e => console.log(e));
+        }
+    
+        const handleTrackEvent=(e)=> {
+            partnerVideo.current.srcObject = e.streams[0];
+        };
+    
+   
         navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
             userVideo.current.srcObject = stream;
             userStream.current = stream;
@@ -37,93 +118,10 @@ const Room = (props) => {
 
             socketRef.current.on("ice-candidate", handleNewICECandidateMsg);
         });
-    }
 
-    const callUser=(userID)=> {
-        peerRef.current = createPeer(userID);
-        userStream.current.getTracks().forEach(track => senders.current.push(peerRef.current.addTrack(track, userStream.current)));
-    }
+    }, []);
 
-    const createPeer=(userID)=> {
-        const peer = new RTCPeerConnection({
-            iceServers: [
-                {
-                    urls: "stun:stun.stunprotocol.org"
-                },
-                {
-                    urls: 'turn:numb.viagenie.ca',
-                    credential: 'muazkh',
-                    username: 'webrtc@live.com'
-                },
-            ]
-        });
-
-        peer.onicecandidate = handleICECandidateEvent;
-        peer.ontrack = handleTrackEvent;
-        peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
-
-        return peer;
-    }
-
-    const handleNegotiationNeededEvent=(userID)=> {
-        peerRef.current.createOffer().then(offer => {
-            return peerRef.current.setLocalDescription(offer);
-        }).then(() => {
-            const payload = {
-                target: userID,
-                caller: socketRef.current.id,
-                sdp: peerRef.current.localDescription
-            };
-            socketRef.current.emit("offer", payload);
-        }).catch(e => console.log(e));
-    }
-
-    const handleRecieveCall=(incoming)=> {
-        peerRef.current = createPeer();
-        const desc = new RTCSessionDescription(incoming.sdp);
-        peerRef.current.setRemoteDescription(desc).then(() => {
-            userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
-        }).then(() => {
-            return peerRef.current.createAnswer();
-        }).then(answer => {
-            return peerRef.current.setLocalDescription(answer);
-        }).then(() => {
-            const payload = {
-                target: incoming.caller,
-                caller: socketRef.current.id,
-                sdp: peerRef.current.localDescription
-            }
-            socketRef.current.emit("answer", payload);
-        })
-    }
-
-    const handleAnswer=(message)=> {
-        const desc = new RTCSessionDescription(message.sdp);
-        peerRef.current.setRemoteDescription(desc).catch(e => console.log(e));
-    }
-
-    const handleICECandidateEvent=(e)=> {
-        if (e.candidate) {
-            const payload = {
-                target: otherUser.current,
-                candidate: e.candidate,
-            }
-            socketRef.current.emit("ice-candidate", payload);
-        }
-    }
-
-    const handleNewICECandidateMsg=(incoming)=> {
-        const candidate = new RTCIceCandidate(incoming);
-
-        peerRef.current.addIceCandidate(candidate)
-            .catch(e => console.log(e));
-    }
-
-    const handleTrackEvent=(e)=> {
-        partnerVideo.current.srcObject = e.streams[0];
-    };
-
-    function shareScreen() {
+    const  shareScreen=() =>{
         navigator.mediaDevices.getDisplayMedia({ cursor: true }).then(stream => {
             const screenTrack = stream.getTracks()[0];
             senders.current.find(sender => sender.track.kind === 'video').replaceTrack(screenTrack);
